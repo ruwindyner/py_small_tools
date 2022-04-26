485fd51d9dd79816ecaa5f94224988798f073111
root <1738620999@qq.com>
2022-04-26 17:10:05 +0800

    practical

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import sys
-from PyQt6.QtWidgets import QApplication,QWidget,QLabel
-from ui import Window
+from PyQt6.QtWidgets import QApplication,QWidget,QLabel,QMainWindow
+from pyqt.Ui_firstmainwin import *
+
+
+class myMainWindow(QMainWindow,Ui_MainWindow):
+    def __init__(self,parent=None):
+        super(myMainWindow,self).__init__(parent)
+        self.setupUi(self)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = Window()
-    window.show()
+    mywin = myMainWindow()
+    mywin.show()
     sys.exit(app.exec())
--- ui.py
+++ ui.py
@@ -1,18 +1,19 @@
+from cProfile import label
 from PyQt6.QtWidgets import QApplication,QWidget,QLabel
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.resize(300,300)
-        self.move(300,300)
-        self.setWindowTitle('first app')
+        self.setWindowTitle('the first app')
+        self.resize(500,500)
+        self.move(200,245)
         self.setup_ui()
     def setup_ui(self):
         label = QLabel(self)
-        label.setText('This is my first app')
-        label.move(100,100)
+        label.setText('this is my first app')
+        label.move(200,245)
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     import sys
     app = QApplication(sys.argv)
     window = Window()
